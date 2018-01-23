4794d18bd8266b00c405a67ed96efeaaf1991839
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-23 11:14:02 +0100

    all files ready up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <h1>LAldajjdasjdadas</h1>
+      </div>
+    )
+  }
+}
+
+export default App
